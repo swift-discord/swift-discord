2ad02251ca10181efa1ff27986e2bda8a8afcf6e
Jaehong Kang <sinoru@me.com>
2022-07-19 19:29:49 +0900

    Initial Project Setup

---
+++ Tests/swift-discordTests/swift_discordTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_discord
+
+final class swift_discordTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_discord().text, "Hello, World!")
+    }
+}
