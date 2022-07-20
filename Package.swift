ad885c5bed83449303099ea3df2ef2cbf5c9aa52
Jaehong Kang <sinoru@me.com>
2022-07-20 21:08:15 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-discord",
+    platforms: [
+         .macOS(.v12),
+         .macCatalyst(.v15),
+         .iOS(.v15),
+         .tvOS(.v15),
+         .watchOS(.v7),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
