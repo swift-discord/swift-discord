3ce5a091d269b5c27636200ac1dfe03faa22e9bc
Jaehong Kang <sinoru@me.com>
2022-07-20 22:07:49 +0900

    Uses specific version of swift-snowflake

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/sinoru/swift-snowflake", branch: "develop"),
+        .package(url: "https://github.com/sinoru/swift-snowflake", .upToNextMinor(from: "0.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
