aaf76ff91571c433f4f6a7a4422ca5d32dfe02b8
Jaehong Kang <sinoru@me.com>
2022-07-24 22:41:09 +0900

    Implement DiscordAPIVersion

---
+++ Sources/DiscordCore/DiscordAPIVersion.swift
@@ -0,0 +1,36 @@
+//
+//  DiscordAPIVersion.swift
+//  
+//
+//  Created by Jaehong Kang on 2022/07/24.
+//
+
+public struct DiscordAPIVersion : Encodable {
+    let versionString: String
+
+    fileprivate init(versionString: String) {
+        self.versionString = versionString
+    }
+
+    @available(*, unavailable, message: "DiscordAPI v6 is the oldest deprecated version")
+    public static let v3: DiscordAPIVersion = .init(versionString: "3")
+
+    @available(*, unavailable, message: "DiscordAPI v6 is the oldest deprecated version")
+    public static let v4: DiscordAPIVersion = .init(versionString: "4")
+
+    @available(*, unavailable, message: "DiscordAPI v6 is the oldest deprecated version")
+    public static let v5: DiscordAPIVersion = .init(versionString: "5")
+
+    @available(*, deprecated, message: "DiscordAPI v9 is the oldest availabled version")
+    public static let v6: DiscordAPIVersion = .init(versionString: "6")
+
+    @available(*, deprecated, message: "DiscordAPI v9 is the oldest availabled version")
+    public static let v7: DiscordAPIVersion = .init(versionString: "7")
+
+    @available(*, deprecated, message: "DiscordAPI v9 is the oldest availabled version")
+    public static let v8: DiscordAPIVersion = .init(versionString: "8")
+
+    public static let v9: DiscordAPIVersion = .init(versionString: "9")
+
+    public static let v10: DiscordAPIVersion = .init(versionString: "10")
+}
